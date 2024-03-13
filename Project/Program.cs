04d6f787bfde498722c200a0a9531d0096b82156
danssolutions <danssolutionshop@gmail.com>
2024-03-13 12:57:54 +0100

    Testing CSV writer

--- Project/Program.cs
+++ Project/Program.cs
@@ -5,18 +5,24 @@
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            JsonAssetManager jsonAssetManager = new();
 
-            string testFilePath = "StaticAssets\\ProductionUnits\\oilBoiler.json";
+            // JsonAssetManager jsonAssetManager = new();
 
-            ProductionUnit testUnit = jsonAssetManager.LoadProductionUnitData(File.ReadAllText(testFilePath));
-            Console.WriteLine("Name: " + testUnit.Name);
-            Console.WriteLine("Image: " + testUnit.ImagePath);
-            Console.WriteLine("MaxHeat: " + testUnit.MaxHeat);
-            Console.WriteLine("MaxElectricity: " + testUnit.MaxElectricity);
-            Console.WriteLine("ProductionCosts: " + testUnit.ProductionCosts);
-            Console.WriteLine("CO2Emissions: " + testUnit.CO2Emissions);
-            Console.WriteLine("GasConsumption: " + testUnit.GasConsumption);
+            // string testFilePath = "StaticAssets\\ProductionUnits\\oilBoiler.json";
+
+            // ProductionUnit testUnit = jsonAssetManager.LoadProductionUnitData(File.ReadAllText(testFilePath));
+            // Console.WriteLine("Name: " + testUnit.Name);
+            // Console.WriteLine("Image: " + testUnit.ImagePath);
+            // Console.WriteLine("MaxHeat: " + testUnit.MaxHeat);
+            // Console.WriteLine("MaxElectricity: " + testUnit.MaxElectricity);
+            // Console.WriteLine("ProductionCosts: " + testUnit.ProductionCosts);
+            // Console.WriteLine("CO2Emissions: " + testUnit.CO2Emissions);
+            // Console.WriteLine("GasConsumption: " + testUnit.GasConsumption);
+
+            ResultDataManager resultDataManager = new();
+            ResultData resultData = new();
+
+            resultDataManager.WriteResultData(resultData);
         }
     }
 }
--- Project/ResultDataManager.cs
+++ Project/ResultDataManager.cs
@@ -1,3 +1,5 @@
+using CsvHelper;
+
 namespace HeatItOn
 {
     public struct ResultData // draft structure
@@ -8,5 +10,35 @@ namespace HeatItOn
         public int NetProductionCosts { get; set; }
         public double CO2Emissions { get; set; }
         public double GasConsumption { get; set; }
+    }
+
+    public class ResultDataManager : IResultDataManager
+    {
+        public ResultData ReadResultData(string data)
+        {
+            ResultData resultData = new();
+            return resultData;
+        }
+        public void WriteResultData(ResultData resultData)
+        {
+            resultData.Name = "Name";
+            resultData.HeatProduction = 0.25;
+            resultData.Electricity = 0.5;
+            resultData.NetProductionCosts = 75;
+            resultData.CO2Emissions = 75.8;
+            resultData.CO2Emissions = 4.5678;
+
+            var records = new List<ResultData>
+            {
+                new() { Name = "Name", HeatProduction = 0.25, Electricity = 0.5, NetProductionCosts = 75, CO2Emissions = 75.8, GasConsumption = 4.5678 },
+                new() { Name = "Name2", HeatProduction = 0.35, Electricity = 0.45, NetProductionCosts = 775, CO2Emissions = 45.8, GasConsumption = 1.23 },
+            };
+
+            using (var writer = new StreamWriter("test.csv"))
+            using (var csv = new CsvWriter(writer, System.Globalization.CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(records);
+            }
+        }
     }
 }
