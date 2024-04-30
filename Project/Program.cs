edaa444bac79e0240e4927693383a112a3350067
Rototype <67031469+Rototype0@users.noreply.github.com>
2024-04-30 12:22:11 +0100

    Updated CSVHelper

--- Project/Program.cs
+++ Project/Program.cs
@@ -10,32 +10,20 @@
             Optimizer optimizer = new();
             
             List<SourceData> data = sourceDataManager.ReadEnerginetAPISourceData().Result;
-
-            // string sourceDataPath = "wintertest"; // can be "summertest" or "wintertest"
-            // List<SourceData> data = sourceDataManager.ReadSourceData(sourceDataPath);
             
-            // foreach (SourceData dataPoint in data)
-            // {
-            //     Console.WriteLine(dataPoint.TimeFrom);
-            //     Console.WriteLine(dataPoint.TimeTo);
-            //     Console.WriteLine(dataPoint.HeatDemand);
-            //     Console.WriteLine(dataPoint.ElectricityPrice);
-            // }
+            foreach (SourceData dataPoint in data)
+            {
+                Console.WriteLine(dataPoint.TimeFrom);
+                Console.WriteLine(dataPoint.TimeTo);
+                Console.WriteLine(dataPoint.HeatDemand);
+                Console.WriteLine(dataPoint.ElectricityPrice);
+            }
             
             List<ProductionUnit> productionUnits = jsonAssetManager.GetAllProductionUnits();
             List<ResultData> writeRecords = optimizer.OptimizeData(productionUnits, data);
 
             string fileName = "ResultDataTest";
             resultDataManager.WriteResultData(writeRecords, fileName);
-
-            // var readRecords = resultDataManager.ReadResultData(fileName);
-            // Console.WriteLine(readRecords[0].ProductionUnitName);
-            // Console.WriteLine(readRecords[0].ProducedHeat);
-            // Console.WriteLine(readRecords[0].NetElectricity);
-            // Console.WriteLine(readRecords[0].ProductionCosts);
-            // Console.WriteLine(readRecords[0].ProducedCO2);
-            // Console.WriteLine(readRecords[0].PrimaryEnergyConsumption);
-            // Console.WriteLine(readRecords[0].OperationPercentage);
         }
     }
 }
