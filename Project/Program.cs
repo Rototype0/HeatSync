2ed7348cfc84e4abae0cf977c99735e48f820329
Rototype <67031469+Rototype0@users.noreply.github.com>
2024-04-30 13:01:20 +0100

    Set up Raylib/Imgui Window

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace HeatItOn
+﻿using Raylib_cs;
+
+namespace HeatItOn
 {
     class Program
     {
@@ -7,10 +9,12 @@
             JsonAssetManager jsonAssetManager = new();
             SourceDataManager sourceDataManager = new();
             ResultDataManager resultDataManager = new();
+            Window MainWindow = new Window();
             Optimizer optimizer = new();
             
             List<SourceData> data = sourceDataManager.ReadEnerginetAPISourceData().Result;
             
+            /*
             foreach (SourceData dataPoint in data)
             {
                 Console.WriteLine(dataPoint.TimeFrom);
@@ -18,12 +22,17 @@
                 Console.WriteLine(dataPoint.HeatDemand);
                 Console.WriteLine(dataPoint.ElectricityPrice);
             }
-            
+            */
             List<ProductionUnit> productionUnits = jsonAssetManager.GetAllProductionUnits();
             List<ResultData> writeRecords = optimizer.OptimizeData(productionUnits, data);
 
             string fileName = "ResultDataTest";
             resultDataManager.WriteResultData(writeRecords, fileName);
+
+            while(!Raylib.WindowShouldClose() && MainWindow.IsImGUIWindowOpen)
+            {
+                MainWindow.Render();
+            }
         }
     }
 }
---
+++ Project/Window.cs
@@ -0,0 +1,76 @@
+﻿using ImGuiNET;
+using System.Numerics;
+using Raylib_cs;
+
+namespace HeatItOn
+{
+    internal class Window
+    {
+        private bool isImGUIWindowOpen;
+
+        internal bool IsImGUIWindowOpen
+        {
+            get { return isImGUIWindowOpen; }
+        }
+
+
+        private ImGuiController controller;
+        private ImGuiWindowFlags ImGuiWindowFlags;
+        private ConfigFlags RaylibWindowFlags;
+
+        internal Window()
+        {
+            Init();
+            Render();
+        }
+        internal void Init()
+        {
+            isImGUIWindowOpen = true;
+            ImGuiWindowFlags = ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoBringToFrontOnFocus;
+            RaylibWindowFlags = ConfigFlags.VSyncHint | ConfigFlags.UndecoratedWindow;
+
+            Raylib.SetConfigFlags(RaylibWindowFlags);
+            Raylib.InitWindow(1280, 720, "Plot");
+            Raylib.SetTargetFPS(60);
+            
+            var context = ImGui.CreateContext();
+            ImGui.SetCurrentContext(context);
+
+            controller = new ImGuiController();
+        }
+        internal void Render()
+        {
+            controller.NewFrame();
+            controller.ProcessEvent();
+
+            InputHandling();
+
+            ImGui.NewFrame();
+            ImGui.SetNextWindowPos(new Vector2(0, 0));
+
+            ImGui.Begin("Test", ref isImGUIWindowOpen, ImGuiWindowFlags);
+            Raylib.SetWindowSize((int)ImGui.GetWindowSize().X, (int)ImGui.GetWindowSize().Y);
+
+            ImGui.Text(Raylib.GetFPS().ToString());
+
+            ImGui.End();
+
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.Blank);
+
+            ImGui.Render();
+            controller.Render(ImGui.GetDrawData());
+            Raylib.EndDrawing();
+
+        }
+
+        private void InputHandling()
+        {
+            if (ImGui.IsMouseDragging(ImGuiMouseButton.Left) && ImGui.GetMousePos().Y <= 20 + ImGui.GetMouseDragDelta().Y)
+            {
+                var CurrentWindowPosition = Raylib.GetWindowPosition();
+                Raylib.SetWindowPosition((int)(CurrentWindowPosition.X + ImGui.GetMouseDragDelta().X), (int)(CurrentWindowPosition.Y + ImGui.GetMouseDragDelta().Y));
+            }
+        }
+    }
+}
